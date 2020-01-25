4e0749bb62afc2aa7f443fc376b265e02a3b364c
Serhii Nerush <nerushok@gmail.com>
2020-01-25 22:44:48 +0200

    Created LiveEventCallback.

--- presentation/src/main/java/io/anonymous/storage/presentation/utils/LiveEventCallback.kt
+++ presentation/src/main/java/io/anonymous/storage/presentation/utils/LiveEventCallback.kt
@@ -5,12 +5,19 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 
-class LiveEventCallback<T>(lifecycleOwner: LifecycleOwner, private val callback: (T?) -> Unit) {
+class LiveEventCallback<T>(
+    lifecycleOwner: LifecycleOwner,
+    private val errorCallback: ((Throwable) -> Unit)? = null,
+    private val callback: (T?) -> Unit
+) {
 
     private val resultLiveData = MutableLiveData<T>()
+    private val errorLiveData = MutableLiveData<Throwable>()
 
     init {
         resultLiveData.observe(lifecycleOwner, Observer { callback(it) })
+
+        if (errorCallback != null) errorLiveData.observe(lifecycleOwner, Observer { errorCallback.invoke(it) })
     }
 
     @MainThread
@@ -20,5 +27,14 @@ class LiveEventCallback<T>(lifecycleOwner: LifecycleOwner, private val callback:
 
     fun post(result: T?) {
         resultLiveData.postValue(result)
+    }
+
+    @MainThread
+    fun error(error: Throwable) {
+        errorLiveData.value = error
+    }
+
+    fun postError(error: Throwable) {
+        errorLiveData.postValue(error)
     }
 }
